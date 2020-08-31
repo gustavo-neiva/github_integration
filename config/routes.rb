c55a350b2eb43877e1082f9ba204a2884a6249bd
Luiz Neiva <luizgzn@gmail.com>
2020-08-31 17:50:51 -0300

    create routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      post '/auth/login', to: 'authentication#create'
+      resources :repositories, only: :index
+      resources :user, except: :index
+      get  'search/repositories', to: 'search_repositories#index'
+      get 'user/:username/repos', to: 'user_repository#index'
+    end
+  end
 end
