b62bebc771e0ffe43d428d4862b5d38a1b18b9a5
Luiz Neiva <luizgzn@gmail.com>
2020-08-31 18:11:08 -0300

    remove postgis extension added by mistake

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,6 @@ ActiveRecord::Schema.define(version: 2020_08_26_170827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "postgis"
-
-  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
-    t.string "auth_name", limit: 256
-    t.integer "auth_srid"
-    t.string "srtext", limit: 2048
-    t.string "proj4text", limit: 2048
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
